15108b16d936ed57301065cdd740ce028183b991
Jacob Sanz-Robinson <47538991+jacobsanz97@users.noreply.github.com>
2022-04-25 22:17:01 -0400

    Add files via upload

---
+++ nervIntegration.py
@@ -0,0 +1,27 @@
+import yaml
+import hashlib
+import subprocess
+
+#Copy the cache files to the server where NeRV can deal with them.
+
+with open('Experiment_Definition.yaml') as file: #Load experiment definition
+    try:
+        experiment_definition  = yaml.safe_load(file)
+    except yaml.YAMLError as exception: #yaml file not valid
+        print('The Experiment Definition file is not valid')
+        print(exception)
+     
+	#Make a hash of the experiment name in the experiment definition, keep first 12 chars   
+    name_string = experiment_definition['Experiment_name']
+    m = hashlib.md5()
+    m.update(name_string.encode("utf-8"))
+    hashed = str(m.hexdigest())[0:12]
+    print(hashed)
+    
+	#Make a unique directory for this experiment, using the hash, copy cache files to it
+    subprocess.call(["mkdir", hashed])
+    cp_command = "cp *.json " + hashed
+    subprocess.call(cp_command, shell=True)
+    
+	#copy files to NeRV server
+    subprocess.call(["scp", "-r", hashed, "ubuntu@206.167.181.134:~/nerv-prototype/nerv/data/"])
