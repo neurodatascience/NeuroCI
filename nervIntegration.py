00630a8bad7836de73ed0a9f9b72f2497f1c7df1
jacobsanz97 <jacobsanz97@hotmail.com>
2022-04-25 22:52:07 -0400

    typo commit test

--- nervIntegration.py
+++ nervIntegration.py
@@ -11,17 +11,17 @@ with open('Experiment_Definition.yaml') as file: #Load experiment definition
         print('The Experiment Definition file is not valid')
         print(exception)
      
-	#Make a hash of the experiment name in the experiment definition, keep first 12 chars   
+    #Make a hash of the experiment name in the experiment definition, keep first 12 chars   
     name_string = experiment_definition['Experiment_name']
     m = hashlib.md5()
     m.update(name_string.encode("utf-8"))
     hashed = str(m.hexdigest())[0:12]
     print(hashed)
     
-	#Make a unique directory for this experiment, using the hash, copy cache files to it
+    #Make a unique directory for this experiment, using the hash, copy cache files to it
     subprocess.call(["mkdir", hashed])
     cp_command = "cp *.json " + hashed
     subprocess.call(cp_command, shell=True)
     
-	#copy files to NeRV server
+    #copy files to NeRV server
     subprocess.call(["scp", "-o", "StrictHostKeyChecking=no", "-r", hashed, "ubuntu@206.167.181.134:~/nerv-prototype/nerv/data/"])
