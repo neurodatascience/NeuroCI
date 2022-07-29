9fd2e943a15894e671526fd0cf60e262c27bb405
Arman <rmanaem@protonmail.ch>
2022-07-28 23:57:02 -0400

    Added slurmAPI module

---
+++ slurmAPI.py
@@ -0,0 +1,23 @@
+
+##################################################################################
+
+def login():
+    pass
+
+def logout():
+    pass
+
+def list_data_provider():
+    pass
+
+def upload():
+    pass
+
+def post_task():
+    pass
+
+def get_all_tasks():
+    pass
+
+def download():
+    pass
