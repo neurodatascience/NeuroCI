18d16c286b822c8e6ca51f9a6db83e70983f7c26
Arman <rmanaem@protonmail.ch>
2022-07-29 12:30:18 -0400

    Implemented login function

--- slurmAPI.py
+++ slurmAPI.py
@@ -1,8 +1,26 @@
+import paramiko
 
 ##################################################################################
 
-def login():
-    pass
+def login(hostname, username, password, port=22):
+    global host
+    host = hostname
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    # Vulnerability to possible MITM attack
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(hostname, port, username, password)
+    except paramiko.BadHostKeyException:
+        print('Server’s host key could not be verified.')
+    except paramiko.AuthenticationException:
+        print('Authentication failed.')
+    except paramiko.SSHException:
+        print('An error occured while connecting\n An SSH session could not be established.')
+    else:
+        print('Connection to', host, 'established.')
+
+        return client
 
 def logout():
     pass
