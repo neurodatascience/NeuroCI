7ad42851df7e50a178feb151c2a2c9c9ec9581d5
Jacob Sanz-Robinson <47538991+jacobsanz97@users.noreply.github.com>
2026-01-05 23:18:12 -0500

    Update main.py

--- neuroci_code/main.py
+++ neuroci_code/main.py
@@ -8,8 +8,8 @@ def main(experiment_definition):
 
     this_experiment = Experiment(experiment_definition)
 
-    for dataset, dataset_path in this_experiment.datasets.items():
-        for pipeline, pipeline_version in this_experiment.pipelines.items():
+ #   for dataset, dataset_path in this_experiment.datasets.items():
+ #       for pipeline, pipeline_version in this_experiment.pipelines.items():
             #this_experiment.update_tracker_info(dataset, dataset_path, pipeline, pipeline_version)
             #this_experiment.run_pipeline(dataset, dataset_path, pipeline, pipeline_version)
     
